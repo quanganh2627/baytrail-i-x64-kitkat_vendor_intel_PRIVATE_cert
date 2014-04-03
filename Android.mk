318b67db89542cb251754ff6ac199ecd1ac113e3
Zhong Chen <zhong.chen@intel.com>
2014-04-03 16:51:08 -0700

    Added Intel CA certs to the list of trusted root certs.
    BZ: 186128
    
    These certificates are copied into /system/etc/security/cacerts.
    
    Change-Id: I34a461108458c57a60b572de88b62ba3a17034c8
    Signed-off-by: Zhong Chen <zhong.chen@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -27,4 +41,38 @@ LOCAL_SIGN ?= true
 ifeq ($(TARGET_OS_SIGNING_METHOD),isu_plat2)
 $(MKBOOTIMG): OS_priv.pem
 endif
+
+#
+# Definitions for installing Certificate Authority (CA) certificates
+#
+
+define all-files-under
+$(patsubst ./%,%, \
+  $(shell cd $(LOCAL_PATH) ; \
+          find $(1) -type f) \
+ )
+endef
+
+# $(1): module name
+# $(2): source file
+define include-prebuilt-cacert
+include $$(CLEAR_VARS)
+LOCAL_MODULE := $(1)
+LOCAL_MODULE_STEM := $(notdir $(2))
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/security/cacerts
+LOCAL_SRC_FILES := $(2)
+include $$(BUILD_PREBUILT)
+endef
+
+cacerts := $(call all-files-under,intel_cacerts)
+
+$(foreach cacert, $(cacerts), $(eval $(call include-prebuilt-cacert,target-cacert-$(notdir $(cacert)),$(cacert))))
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := intel_cacerts
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := $(foreach cacert, $(cacerts), target-cacert-$(notdir $(cacert)))
+include $(BUILD_PHONY_PACKAGE)
 
